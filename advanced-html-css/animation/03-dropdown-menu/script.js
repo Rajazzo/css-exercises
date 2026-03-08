bfe5908522ae42d3fef74955a41e533bdb3eaa44
Rajazzo <ralf.bauer-malz@kabelbw.de>
2026-03-08 10:41:21 +0100

    accomplished advanced-html-css/animation/03-dropdown-menu

--- advanced-html-css/animation/03-dropdown-menu/script.js
+++ advanced-html-css/animation/03-dropdown-menu/script.js
@@ -1,15 +1,18 @@
 const dropdownContainer = document.querySelector(".dropdown-container");
 const menuTitle = document.querySelector(".menu-title");
 const dropdownMenu = document.querySelector(".dropdown-menu");
+const test = document.querySelector(".test");
 
 menuTitle.addEventListener("click", (e) => {
   if (e.target === e.currentTarget) {
     dropdownMenu.classList.toggle("visible");
+    test.classList.toggle("visible");
   }  
 })
 
 window.addEventListener("click", (e) => {
   if (!dropdownContainer.contains(e.target)) {
     dropdownMenu.classList.remove("visible")
+    test.classList.remove("visible");
   }
 })
